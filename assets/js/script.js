17771c32f0ffcb6ec9d8d316c4ef16bb08a9cef5
Waffles <knharman@gmail.com>
2021-12-11 11:10:37 -0800

    api calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,50 @@ var handleSearchButtonClick = function (event) {
     displayCityWeatherData(cityInput.value)
 }
 
-var displayCityWeatherData = function (cityName) {
-    getLatLon(cityName);
+var displayCityWeatherData = async function (cityName) {
+    getLatLon(cityName)
+    .then(function(latLon) {
+        return getWeatherData(latLon);
+    })
+    .then(function(weatherData) {
+        updateContainers(weatherData);
+    });
+}
+
+var updateContainers = function (weatherData) {
+    console.log(weatherData);
 }
 
 var getLatLon = function(cityName) {
     // api.openweathermap.org/data/2.5/weather?q=portland&appid=e0c1ba6410238103fe2d482d8b1d932f
     var appId = "e0c1ba6410238103fe2d482d8b1d932f"
     var url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${appId}`
-    fetch(url)
+    return fetch(url)
         .then(function (response) {
             // request was successful
-            if (response.ok) {
-                response.json().then(function (data) {
-                    console.log(data);
-                });
-            } else {
-                alert('Error: GitHub User Not Found');
-            }
+            return response.json();
+        })
+        .then(function (responseData) {
+            return responseData.coord;
         })
         .catch(function (error) {
             // Notice this `.catch()` getting chained onto the end of the `.then()` method
-            alert("Unable to connect to GitHub");
+            alert("Unable to fetch latitude and longitude data.");
         });
+}
+
+var getWeatherData = function(latLon) {
+    var appId = "e0c1ba6410238103fe2d482d8b1d932f"
+    var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latLon.lat}&lon=${latLon.lon}&appid=${appId}`;
+    
+    return fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (responseData) {
+            return responseData;
+        })
+        .catch(function (error) {
+            alert("Unable to fetch weather data.")
+        })
 }
